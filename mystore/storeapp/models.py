ba4a6f1ce9240949e479737d9a4bb10131aaa999
Ariellles <marielkago@hotmail.com>
2022-04-23 11:37:34 +0300

    Adding a model

--- mystore/storeapp/models.py
+++ mystore/storeapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    size = models.CharField(max_length=20) #Small or Normal
+    colour = models.CharField(max_length=20)
+    material = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
